ed1508b15662a9e35c18705f2345be73a9e6821f
zacklocx <zacklocx@gmail.com>
2018-09-24 00:29:20 +0800

    improve timer interface

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,27 +1,49 @@
 
 #include "timer.h"
 
+#include <chrono>
+#include <utility>
+
 #include <boost/bind.hpp>
 
-void timer_t::start()
+timer_t::timer_t(boost::asio::io_service& service, int period_ms, handler_type handler)
+		: period_ms_(period_ms), count_(0), limit_(0), running_(false)
+		, handler_(std::move(handler))
+		, timer_(std::make_shared<boost::asio::steady_timer>(std::ref(service), std::chrono::milliseconds(period_ms)))
+{}
+
+int timer_t::period() const
+{
+	return period_ms_;
+}
+
+uint64_t timer_t::count() const
+{
+	return count_;
+}
+
+uint64_t timer_t::limit() const
+{
+	return limit_;
+}
+
+bool timer_t::running() const
+{
+	return running_;
+}
+
+void timer_t::run(uint64_t limit /* = 0 */)
 {
 	if(!running_)
 	{
+		limit_ = limit;
 		running_ = true;
+
 		timer_->async_wait(boost::bind(&timer_t::handle_wait, this, boost::asio::placeholders::error));
 	}
 }
 
 void timer_t::stop()
-{
-	if(running_)
-	{
-		running_ = false;
-		timer_->cancel();
-	}
-}
-
-void timer_t::reset()
 {
 	if(running_)
 	{
@@ -38,12 +60,19 @@ void timer_t::handle_wait(const boost::system::error_code& ec)
 	{
 		++count_;
 
-		timer_->expires_at(timer_->expires_at() + std::chrono::milliseconds(period_ms_));
-		timer_->async_wait(boost::bind(&timer_t::handle_wait, this, boost::asio::placeholders::error));
-
 		if(handler_)
 		{
 			handler_(period_ms_, count_);
+		}
+
+		if(0 == limit_ || count_ < limit_)
+		{
+			timer_->expires_at(timer_->expires_at() + std::chrono::milliseconds(period_ms_));
+			timer_->async_wait(boost::bind(&timer_t::handle_wait, this, boost::asio::placeholders::error));
+		}
+		else
+		{
+			stop();
 		}
 	}
 }
--- src/timer.h
+++ src/timer.h
@@ -4,9 +4,7 @@
 
 #include <cstdint>
 
-#include <chrono>
 #include <memory>
-#include <utility>
 #include <functional>
 
 #include <boost/asio.hpp>
@@ -17,23 +15,19 @@ class timer_t
 public:
 	typedef std::function<void(int, uint64_t)> handler_type;
 
-	timer_t(boost::asio::io_service& service, int period_ms, handler_type handler)
-		: period_ms_(period_ms), count_(0), running_(false)
-		, handler_(std::move(handler))
-		, timer_(std::make_shared<boost::asio::steady_timer>(std::ref(service), std::chrono::milliseconds(period_ms)))
-	{}
+	timer_t(boost::asio::io_service& service, int period_ms, handler_type handler);
 
-	int period() const { return period_ms_; }
-	uint64_t count() const { return count_; }
-	bool running() const { return running_; }
+	int period() const;
+	uint64_t count() const;
+	uint64_t limit() const;
+	bool running() const;
 
-	void start();
+	void run(uint64_t limit = 0);
 	void stop();
-	void reset();
 
 private:
 	int period_ms_;
-	uint64_t count_;
+	uint64_t count_, limit_;
 	bool running_;
 
 	handler_type handler_;
